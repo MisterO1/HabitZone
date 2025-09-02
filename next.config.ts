6031c206f805cbd65b4166cc9ee6ee4ca42f20fa
misterO <mistero4321@gmail.com>
2025-09-02 16:50:44 +0200

    add output:export

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingRoot: __dirname,
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+  output: "export"
 };
 
 export default nextConfig;
